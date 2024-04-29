ce5e713be73c78aa14e41f2b88025c053c8e5518
JCZ <615315510@qq.com>
2024-04-29 14:33:54 +0800

    android demo 优化

--- android-native-java/generic-classic-buletooth-demo/app/src/main/java/com/example/classic_bluetooth_demo/ScanActivity.java
+++ android-native-java/generic-classic-buletooth-demo/app/src/main/java/com/example/classic_bluetooth_demo/ScanActivity.java
@@ -15,7 +15,6 @@ import android.os.Process;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
