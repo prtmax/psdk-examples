21367230387cedf48733847396acf0e6c41caa49
JCZ <615315510@qq.com>
2025-05-22 11:19:32 +0800

    鸿蒙demo新增状态查询

--- harmony-demo/entry/src/main/ets/common/ReadMark.ts
+++ harmony-demo/entry/src/main/ets/common/ReadMark.ts
@@ -1,5 +1,5 @@
 
-export enum EscQueryType {
+export enum ReadMark {
   Info = 0,             // 查询打印机信息
   SN,               // 查询打印机SN
   Mac,              // 查询MAC地址
@@ -7,21 +7,18 @@ export enum EscQueryType {
   Version,          // 查询打印机固件版本
   Model,            // 查询打印机型号
   BatteryVolume,    // 查询电量
-  ShutTime,         // 查询关机时间
+  SetShutTime,         // 查询关机时间
   BtName,           // 查询BT名称
+  Thickness,   // 设置浓度
+  GetShutTime,    // 设置关机时间
+  PaperType,   // 设置纸张类型
+  // LabelGap,    // 设置学习标签缝隙
   // PaperType,        // 查询纸张类型
   // BtVersion,        // 查询BT版本
   // NfcPaper,         // 查询纸张信息
   // NfcUID,           // 获取标签UID
   // NfcUsedLength,    // 获取标签使用长度
   // NfcRestLength,    // 获取标签剩余长度
-}
-
-export enum EscSettingType {
-  Thickness,   // 设置浓度
-  ShutTime,    // 设置关机时间
-  PaperType,   // 设置纸张类型
-  // LabelGap,    // 设置学习标签缝隙
 }
 
 export enum ThicknessLevel {
