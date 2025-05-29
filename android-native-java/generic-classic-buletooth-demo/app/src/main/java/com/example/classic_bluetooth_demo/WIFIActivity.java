649957ae15835e4fe739c92d3136574729a61547
JCZ <615315510@qq.com>
2025-05-29 14:10:11 +0800

    android Wi-Fi set

--- android-native-java/generic-classic-buletooth-demo/app/src/main/java/com/example/classic_bluetooth_demo/WIFIActivity.java
+++ android-native-java/generic-classic-buletooth-demo/app/src/main/java/com/example/classic_bluetooth_demo/WIFIActivity.java
@@ -18,15 +18,20 @@ import com.printer.psdk.frame.father.listener.ListenAction;
 import com.printer.psdk.wifi.GenericWIFI;
 import com.printer.psdk.wifi.WIFI;
 import com.printer.psdk.wifi.args.WSetSSID;
+import com.printer.psdk.wifi.args.WSetWifiIP;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WIFIActivity extends Activity {
   private EditText wifi_name, wifi_pwd;
-  private Button button_send;
-  private Button button_search_name;
-  private Button button_search_pwd;
-  private Button button_status;
+  private EditText etIpAddress, etGateway, etNetmask;
+  private Button button_send, button_search_name, button_search_pwd, button_status, button_search_dhcp;
+  private Button btnSetIp, btnGetIp;
+  private RadioGroup rgIpType;
+  private RadioButton dhcp_radio, static_ip_radio;
+  private View staticIpSettings;
   private TextView tv_content;
   private TextView title_right_text;
   private Connection connection;
@@ -45,6 +50,16 @@ public class WIFIActivity extends Activity {
     button_search_pwd = (Button) findViewById(R.id.button_search_pwd);
     button_status = (Button) findViewById(R.id.button_status);
     tv_content = (TextView) findViewById(R.id.tv_content);
+    button_search_dhcp = (Button) findViewById(R.id.button_search_dhcp);
+    rgIpType = findViewById(R.id.ip_type_radio_group);
+    dhcp_radio = findViewById(R.id.dhcp_radio);
+    static_ip_radio = findViewById(R.id.static_ip_radio);
+    staticIpSettings = findViewById(R.id.static_ip_settings);
+    etIpAddress = findViewById(R.id.ip_address);
+    etGateway = findViewById(R.id.gateway);
+    etNetmask = findViewById(R.id.netmask);
+    btnSetIp = findViewById(R.id.button_set_ip);
+    btnGetIp = findViewById(R.id.button_get_ip);
     BluetoothDevice device = getIntent().getParcelableExtra("device");
     connection = Bluetooth.getInstance().createConnectionClassic(device, new ConnectListener() {
       @Override
@@ -128,6 +143,14 @@ public class WIFIActivity extends Activity {
         safeWrite(_gesc);
       }
     });
+    button_search_dhcp.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        readMark = ReadMark.OPERATE_WIFI_DHCP;
+        GenericWIFI _gesc = wifi.getWifiDHCP();
+        safeWrite(_gesc);
+      }
+    });
     button_send.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
@@ -142,8 +165,60 @@ public class WIFIActivity extends Activity {
         }
       }
     });
+    btnSetIp.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if(dhcp_radio.isChecked()){
+          GenericWIFI _gesc = wifi.setWifiDHCP();//设置成动态ip
+          safeWrite(_gesc);
+          return;
+        }
+        String ip = etIpAddress.getText().toString();
+        String mask = etNetmask.getText().toString();
+        String gateway = etGateway.getText().toString();
+        if (!isValidInput(ip)) {
+          Toast.makeText(WIFIActivity.this, "检查输入的ip", Toast.LENGTH_LONG).show();
+          return;
+        }
+        if (!isValidInput(mask)) {
+          Toast.makeText(WIFIActivity.this, "检查输入的子网掩码", Toast.LENGTH_LONG).show();
+          return;
+        }
+        if (!isValidInput(gateway)) {
+          Toast.makeText(WIFIActivity.this, "检查输入的网关", Toast.LENGTH_LONG).show();
+          return;
+        }
+        //设置成静态ip需传入相关信息
+        GenericWIFI _gesc = wifi.setWifiIP(WSetWifiIP.builder().ip(ip).mask(mask).gateway(gateway).build());
+        safeWrite(_gesc);
+      }
+    });
+    btnGetIp.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        readMark = ReadMark.OPERATE_ALL_IP_INFO;
+        GenericWIFI _gesc = wifi.getWifiIP().getWifiMASK().getWifiGATEWAY();//查询相关信息
+        safeWrite(_gesc);
+      }
+    });
+    rgIpType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == R.id.static_ip_radio) {
+          staticIpSettings.setVisibility(View.VISIBLE);
+        } else {
+          staticIpSettings.setVisibility(View.GONE);
+        }
+      }
+    });
+  }
 
-
+  /// 校验格式
+  private boolean isValidInput(String input) {
+    final String IP_REGEX = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+    Pattern pattern = Pattern.compile(IP_REGEX);
+    Matcher matcher = pattern.matcher(input);
+    return matcher.matches();
   }
 
   private void dataListen(ConnectedDevice connectedDevice) {
@@ -182,11 +257,56 @@ public class WIFIActivity extends Activity {
               } catch (Exception e) {
                 e.printStackTrace();
               }
-              String content = "wifi连接状态:" + linkState;
+              boolean wifiConnected = false;
+              if (linkState.contains("GETLINKSTATE:")) {
+                String status = linkState.replaceAll("GETLINKSTATE:", "").split(",")[0];
+                wifiConnected = status.equals("Connected");
+              }
+              String content = "wifi连接状态:" + (wifiConnected ? "已连接" : "未连接");
               runOnUiThread(() -> tv_content.setText(content));
               break;
+            case OPERATE_WIFI_DHCP:
+              readMark = ReadMark.NONE;
+              String dhcp = "";
+              try {
+                dhcp = new String(received, "GB2312");
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+              boolean isDHCP = dhcp.replace("\r\n", "").equals("1");
+              String contentDHCP = "DHCP:" + (isDHCP ? "动态" : "静态");
+              runOnUiThread(() -> {
+                if (isDHCP) {
+                  dhcp_radio.setChecked(true);
+                } else {
+                  static_ip_radio.setChecked(true);
+                }
+                tv_content.setText(contentDHCP);
+              });
+              break;
+            case OPERATE_ALL_IP_INFO:
+              readMark = ReadMark.NONE;
+              String ipInfo = "";
+              try {
+                ipInfo = new String(received, "GB2312");
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+              String[] infos = ipInfo.split("\r\n");
+              if (infos.length > 2) {
+                String ip = infos[0];
+                String mask = infos[1];
+                String gateway = infos[2];
+                String contentIpInfo = "ip:" + ip + "\n" + "子网掩码:" + mask + "\n" + "网关:" + gateway;
+                runOnUiThread(() -> {
+                  tv_content.setText(contentIpInfo);
+                  etIpAddress.setText(ip);
+                  etNetmask.setText(mask);
+                  etGateway.setText(gateway);
+                });
+              }
+              break;
             default:
-
               break;
           }
         }
